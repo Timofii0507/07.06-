a1f8db7b0803b1a759a81dd27063676c86c93179
Юрков Тимофій Олександрович <145855201+Timofii0507@users.noreply.github.com>
2024-08-07 14:15:32 +0300

    lab/ex-1

--- 07.06/Program.cs
+++ 07.06/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text;
+using System.Threading;
+
+namespace system2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.CursorVisible = false;
+            Thread numberThread = new Thread(PrintNumbers);
+
+
+            numberThread.Start();
+
+
+            numberThread.Join();
+
+            Console.WriteLine("Всі числа виведено.");
+        }
+
+        static void PrintNumbers()
+        {
+            for (int i = 0; i <= 50; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
