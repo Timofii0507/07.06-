7c7f6528b26a6ba0398e877ba59d1b3df920f192
Юрков Тимофій Олександрович <145855201+Timofii0507@users.noreply.github.com>
2024-08-07 14:21:24 +0300

    lab/ex-1

--- 07.06/Program.cs
+++ 07.06/Program.cs
@@ -2,35 +2,41 @@
 using System.Text;
 using System.Threading;
 
-namespace system2
-{
+namespace _07._06
+{ 
     class Program
+{
+    static void Main(string[] args)
     {
+        Console.InputEncoding = Encoding.Unicode;
+        Console.OutputEncoding = Encoding.Unicode;
+        Console.ForegroundColor = ConsoleColor.DarkBlue;
+        Console.CursorVisible = false;
+        Console.Write("Введіть початок діапазону: ");
+        int start = int.Parse(Console.ReadLine());
 
-        static void Main(string[] args)
+        Console.Write("Введіть кінець діапазону: ");
+        int end = int.Parse(Console.ReadLine());
+
+
+        Thread numberThread = new Thread(() => PrintNumbers(start, end));
+
+
+        numberThread.Start();
+
+
+        numberThread.Join();
+
+        Console.WriteLine("Всі числа виведено.");
+    }
+
+    static void PrintNumbers(int start, int end)
+    {
+        for (int i = start; i <= end; i++)
         {
-            Console.InputEncoding = Encoding.Unicode;
-            Console.OutputEncoding = Encoding.Unicode;
-            Console.ForegroundColor = ConsoleColor.DarkBlue;
-            Console.CursorVisible = false;
-            Thread numberThread = new Thread(PrintNumbers);
-
-
-            numberThread.Start();
-
-
-            numberThread.Join();
-
-            Console.WriteLine("Всі числа виведено.");
-        }
-
-        static void PrintNumbers()
-        {
-            for (int i = 0; i <= 50; i++)
-            {
-                Console.WriteLine(i);
-                Thread.Sleep(100);
-            }
+            Console.WriteLine(i);
+            Thread.Sleep(100);
         }
     }
+}
 }
